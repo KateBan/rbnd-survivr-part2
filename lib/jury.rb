0e9e8fd9053ae2edaa8491d551459d76e56dcb2d
Katerina <kbaneva@gmail.com>
2016-03-09 13:12:25 +0200

    jury_test finished

--- lib/jury.rb
+++ lib/jury.rb
@@ -19,15 +19,25 @@ class Jury
 		finalists_votes = {}
 		finalists.each {|finalist| finalists_votes[finalist] = 0}
 		@members.each do |member|
-			vote = finalists.sample
+			votes = finalists.sample
 			puts "#{member}"
-			finalists_votes[vote] += 1
+			finalists_votes[votes] += 1
 		end
 		return finalists_votes
+	end
 
 	def report_votes(final_votes)
-	end
+		final_votes.each {|key,votes| puts "#{key.to_s.capitalize} got #{votes} votes"}
 	end
 
+	def announce_winner(final_votes)
+		final_votes.each do |key, votes|
+			if votes >= 4
+				puts "The winner is #{key}"
+				return key
+			end
+	 	end
+	end
+	
 
 end
